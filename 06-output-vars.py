8e4397890bfb92a328f8d8f0f7444a0f1bd6399d
Spandan Manna <send2spandan@gmail.com>
2021-10-26 11:56:50 +0530

    first commit

---
+++ 06-output-vars.py
@@ -0,0 +1,10 @@
+# Output Variables
+"""
+The Python print statement is often used to output variables.
+To combine both text and a variable, Python uses the + character:
+
+"""
+
+# Example
+x = "awesome"
+print("Python is " + x)
---
+++ 08-data-types.py
@@ -0,0 +1,4 @@
+"""
+This file is under developement stage.
+As an alternative, please visit - https://www.w3schools.com/python/python_datatypes.asp
+"""
